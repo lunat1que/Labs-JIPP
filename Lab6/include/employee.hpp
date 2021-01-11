7b2afa3fc6a7e8ed58bac6effe166644e65d8aa9
lunat1que <72377920+lunat1que@users.noreply.github.com>
2021-01-11 10:30:06 +0200

    Update employee.hpp

--- Lab6/include/employee.hpp
+++ Lab6/include/employee.hpp
@@ -1,1 +1,31 @@
+#pragma once
 
+#include <string>
+using namespace std;
+
+
+class Employee
+{
+    public:
+    Employee();
+    Employee(string id, string name, string surname, string departmentId, string position);
+
+    string GetId() const;
+    void SetId(string);
+    string GetName() const;
+    void SetName(string);
+    string GetSurname() const;
+    void SetSurname(string);
+    string GetDepartmentId() const;
+    void SetDepartmentId(string);
+    string GetPosition() const;
+    void SetPosition(string);
+
+    private:
+    string id;
+    string name;
+    string surname;
+    string departmentId;
+    string position;
+
+};
