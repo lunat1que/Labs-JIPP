68f05a245ca57751825429e4fe0cf5d823884d46
lunatiqueue <72377920+lunatiqueue@users.noreply.github.com>
2021-01-22 00:04:34 +0200

    Update matrix.hpp

--- Lab5/include/matrix.hpp
+++ Lab5/include/matrix.hpp
@@ -1,1 +1,36 @@
+#ifndef MAT_HPP
+#define MAT_HPP
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include "wyjatki.hpp"
 
+class Matrix
+{
+    private:
+        double ** arr;
+        int n_class {}, m_class {};
+    public:
+        
+        int rows() const;
+        int cols() const;
+        Matrix(int n, int m);
+        Matrix(int n);
+        Matrix(string filename, string path);
+        void set(int n, int m, double val);
+        double get(int x, int y) const;
+        Matrix operator +(const Matrix& m2);
+        Matrix operator -(const Matrix& m2);
+        Matrix operator *(const Matrix& m2);
+        Matrix operator >>(int n);
+        void operator +=(const Matrix& m2);
+        void operator -=(const Matrix& m2);
+        bool operator ==(const Matrix& m2);
+        void operator ++(int inc);
+        void operator [](int n);
+        void store(string filename,string path);
+        void print();
+};
+
+ostream& operator<<(ostream& os, const Matrix& m2);
+#endif
